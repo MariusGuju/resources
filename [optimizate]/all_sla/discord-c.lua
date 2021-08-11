24a41040b73f93b24ac390f08dfac30d706d8552
Kasy420 <lolecasi2@yahoo.com>
2021-08-12 00:26:58 +0300

    Update discord-c.lua

--- [optimizate]/all_sla/discord-c.lua
+++ [optimizate]/all_sla/discord-c.lua
@@ -24,7 +24,7 @@ SetDiscordAppId(831533382009094175)-- Discord RICH ID
 SetDiscordRichPresenceAsset('liquid_logo') -- IMAGEM PNG ARQUIVO
 SetDiscordRichPresenceAssetText('Liquid Romania RolePlay') -- PNG DESCRI��O 1 TEXTO
 SetDiscordRichPresenceAssetSmall('liquid_logo')
-SetDiscordRichPresenceAssetSmallText('discord.io/discord.io/LiquidRR') 
+SetDiscordRichPresenceAssetSmallText('discord.io/LiquidRR') 
 SetDiscordRichPresenceAction(0, "Server Discord", "https://discord.gg/discord.io/LiquidRR")
 SetDiscordRichPresenceAction(1, "JOIN FIVEM", "fivem://connect/cfx.re/")
 --SetRichPresence("[ID:" ..user_id.."] - [Nume:"..name.. "] - ".. #players .. "/32 - [Job:" .. faction .. "]" )
