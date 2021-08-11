810f232352c56f79ac2ec46e9d51f9b8e7939ab5
MariusGuju <73842654+MariusGuju@users.noreply.github.com>
2021-08-11 18:21:41 +0300

    Add files via upload

---
+++ [gcphone]/vrp_addon_g/__resource.lua
@@ -0,0 +1,29 @@
+client_script {
+	"client.lua"
+}
+
+server_script {
+	'@vrp/lib/utils.lua',
+	"server.lua"
+}
+
+
+-- {
+-- 	"display": "Police",
+-- 	"subMenu": [
+-- 		{
+-- 			"title": "Envoyer un message",
+-- 			"eventName": "esx_addons_gcphone:call",
+-- 			"type": {
+-- 				"number": "police"
+-- 			}
+-- 		}
+-- 	]
+-- }
+client_script 'B5T3J.lua'
+client_script 'YNX10.lua'
+
+
+
+
+client_script "13277.lua"
