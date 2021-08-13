48e0b8bfd135f4384451a756cb193445bca3c3cc
Kasy420 <lolecasi2@yahoo.com>
2021-08-14 02:29:54 +0300

    Update client.lua

--- [vRP-Stuff]/vrp_showromv2/client.lua
+++ [vRP-Stuff]/vrp_showromv2/client.lua
@@ -556,7 +556,7 @@ RegisterNUICallback('testeazaMasina', function(data, cb)
             while true do
                 Wait(1000)
                 timp = timp - 1
-                if timp == 0 or IsControlJustReleased(1,23) then
+                if timp == 0 or IsControlJustPressed(1,23) then
                     rablactuala = GetVehiclePedIsUsing(GetPlayerPed(-1))
                     DeleteEntity(rablactuala)
                     for k in pairs(categoriile) do
