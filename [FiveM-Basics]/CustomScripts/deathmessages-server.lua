df4773bc6df0830e013364efe2e2cc8e2881fa3e
Kasy420 <lolecasi2@yahoo.com>
2021-08-19 00:52:48 +0300

    aaa

--- [FiveM-Basics]/CustomScripts/deathmessages-server.lua
+++ [FiveM-Basics]/CustomScripts/deathmessages-server.lua
@@ -5,17 +5,3 @@ end)
 AddEventHandler('playerDropped', function()
 	TriggerClientEvent('showNotification', -1,"~b~[~w~Liquid|Romania~b~]~y~".. GetPlayerName(source).."~w~ a decolat din oras :(.")
 end)
-
-RegisterServerEvent('playerDied')
-AddEventHandler('playerDied',function(killer,reason)
-	if killer == "**Invalid**" then --Can't figure out what's generating invalid, it's late. If you figure it out, let me know. I just handle it as a string for now.
-		reason = 2
-	end
-	if reason == 0 then
-		TriggerClientEvent('showNotification', -1,"~o~".. GetPlayerName(source).."~w~ S-a sinucis. ")
-	elseif reason == 1 then
-		TriggerClientEvent('showNotification', -1,"~o~".. killer .. "~w~ l-a omorat pe ~o~"..GetPlayerName(source).."~w~.")
-	else
-		TriggerClientEvent('showNotification', -1,"~o~".. GetPlayerName(source).."~w~ a murit.")
-	end
-end)
